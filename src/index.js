759db13f8530d2afaf8445e94e3a11366482f0f6
o1901k <kristina.oleshchuk@gmail.com>
2022-02-24 22:38:31 +0300

    feat solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+     let brackets = bracketsConfig.join('').replace(/,/g, '');
+      let array = [];
+  for (let bracket of str) {
+          let bracketsIndex = brackets.indexOf(bracket)
+          if (bracketsIndex % 2 === 0) {
+              if (bracket === brackets[bracketsIndex + 1] && array[array.length - 1] === bracketsIndex){
+                  array.pop();
+              } else if (bracket === brackets[bracketsIndex + 1] && array[array.length - 1] !== bracketsIndex) {
+                  array.push(bracketsIndex)
+              }
+              else{
+                  array.push(bracketsIndex)
+              }
+          } 
+          else {
+            if (array.pop() !== bracketsIndex-1) {
+                  return false;
+              }
+          }
+      }
+      return array.length === 0
+  }
